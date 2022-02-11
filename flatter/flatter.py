3d85abbf7d053ea621a0a6b777609d45cb3f5914
Cristian Angulo Nova <cristianangulonova@gmail.com>
2022-02-10 21:42:46 -0500

    feat: Adding more TestCases

--- flatter/flatter.py
+++ flatter/flatter.py
@@ -1,1 +1,4 @@
+"""
+This transform any list object with N dimensions in a flat list
+"""
 flatter = lambda obj: [element for item in obj for element in flatter(item)] if isinstance(obj, list) else [obj]
